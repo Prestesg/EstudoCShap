94e2e1aa6d56045bfc18a59f827f56d8e4ff02df
Gabriel Prestes <gabiprestess@hotmail.com>
2018-08-16 22:04:47 -0300

    Abordagem DatabaseFist importando base do Adventure Works 2016

---
+++ AulaCshap/EFDatabaseFirst/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDatabaseFirst.Controllers
+{
+    class PersonController
+    {
+        AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+        public void Inserir(Person p)
+        {
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var pessoa = BuscarPorID(id);
+            if (pessoa != null)
+            {
+                contexto.Person.Remove(pessoa);
+                contexto.SaveChanges();
+            }
+
+        }
+
+        public List<Person> Listar()
+        {
+            return contexto.Person.ToList();
+        }
+
+        public Person BuscarPorID(int id)
+        {
+            return contexto.Person.Find(id);
+        }
+
+        public Person Update(Person p)
+        {
+            
+        }
+    }
+}
